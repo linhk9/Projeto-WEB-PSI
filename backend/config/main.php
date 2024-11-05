9d6170435c00cbccbdd1ed7dfb8d18e74cd6279e
linhk9 <linhk9.pereira@gmail.com>
2024-11-05 02:50:45 +0000

    fix

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,13 +13,6 @@ return [
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
-        'view' => [
-            'theme' => [
-                'pathMap' => [
-                    '@app/views' => '@vendor/hail812/yii2-adminlte3/src/views'
-                ],
-            ],
-        ],
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
